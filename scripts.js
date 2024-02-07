65090f382a0c1ea783543115cddc0b7d041c48af
cwastian <40618326+cwastian@users.noreply.github.com>
2024-02-07 01:10:01 -0300

    phone touch scripts.js

--- scripts.js
+++ scripts.js
@@ -116,6 +116,15 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
+document.addEventListener("touchstart", function (event) {
+  event.preventDefault();
+  if (bird.y > 0) {
+    bird.flap();
+    if (!gameInProgress) {
+      startGame();
+    }
+  }
+
 const backgroundImg = new Image();
 backgroundImg.src = "lake.jfif";
 backgroundImg.onload = function () {
